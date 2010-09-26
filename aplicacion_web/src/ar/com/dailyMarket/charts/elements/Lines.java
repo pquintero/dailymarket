b71285348e3f118b028070c3d13cfec315047921
epallich <epallich@3a64d12f-38c2-6f80-d3d8-c9ae7da45978>
2010-09-26 22:26:20 +0000

    Limpieza

--- aplicacion_web/src/ar/com/dailyMarket/charts/elements/Lines.java
+++
@@ -1,6 +0,0 @@
-package ar.com.dailyMarket.charts.elements;
-
-//Solo para poder juntar CategoryElement con VLines o setChartElement con VLines
-public interface Lines {
-	public String elementName();
-}
