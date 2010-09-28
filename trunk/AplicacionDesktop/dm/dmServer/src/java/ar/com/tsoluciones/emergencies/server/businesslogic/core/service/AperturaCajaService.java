702a8f290b06870f70c6eef414d98a5e3c78c071
gabrielottaviano@gmail.com <gabrielottaviano@gmail.com@3a64d12f-38c2-6f80-d3d8-c9ae7da45978>
2010-09-28 01:16:30 +0000

    obtengo el string de la huella y se lo seteo al usuario, falta persistir el update

--- trunk/AplicacionDesktop/dm/dmServer/src/java/ar/com/tsoluciones/emergencies/server/businesslogic/core/service/AperturaCajaService.java
+++ trunk/AplicacionDesktop/dm/dmServer/src/java/ar/com/tsoluciones/emergencies/server/businesslogic/core/service/AperturaCajaService.java
@@ -1,5 +1,9 @@
 package ar.com.tsoluciones.emergencies.server.businesslogic.core.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.prefs.AbstractPreferences;
+import java.util.prefs.Preferences;
 
 import ar.com.tsoluciones.arcom.logging.Log;
 import ar.com.tsoluciones.arcom.security.User;
@@ -26,22 +30,48 @@ public class AperturaCajaService implements AperturaCajaServiceInterface {
 		return userToAuthenticate.authenticate(huellaDigital);
 	}
 
-	public boolean isFirstLogin(String username, String password) {
-		
-		UserServiceInterface userServiceInterface = new UserService();
-		User user = userServiceInterface.getUserByUserName(username);
-		if(user.getHuelladigital() == null || user.getHuelladigital().length == 0)
-			return true;
-		else 
-			return false;
-	}
 	
 	public boolean altaHuellaDigital(String username, String password, String huella ){
 		
-		if(isFirstLogin(username, password))
-			
-			
-		;
-		return true;
+		UserServiceInterface userServiceInterface = new UserService();
+		User user = userServiceInterface.getUserByUserName(username);
+		
+		byte[] huellaByte= null;
+		// Es primer logueo?
+		if(user.getHuelladigital() == null || user.getHuelladigital().length == 0){
+			huellaByte = MyBase64.decode(huella);
+			user.setHuelladigital(huellaByte);
+		//TODO updatear usuario
+			return true;
+		}
+		return false;
 	}
+
+	public static class MyBase64 {
+	     private static class MyPreferences extends AbstractPreferences {
+	         private Map<String,String> map = new HashMap<String,String>();
+	         MyPreferences() { super(null,""); }
+	         protected void putSpi(String key,String value) { map.put(key,value); }
+	         protected String getSpi(String key) { return map.get(key); }
+	         protected void removeSpi(String key) { map.remove(key); }
+	         protected void removeNodeSpi() { }
+	         protected String[] keysSpi() { return null; }
+	         protected String[] childrenNamesSpi() { return null; }
+	         protected AbstractPreferences childSpi(String key) { return null; }
+	         protected void syncSpi() {}
+	         protected void flushSpi() {}
+	     }
+	     static String encode(byte[] ba) {
+	         Preferences p = new MyPreferences();
+	         p.putByteArray("",ba);
+	         return p.get("",null);
+	     }
+	     static byte[] decode(String s) {
+	         Preferences p = new MyPreferences();
+	         p.put("",s);
+	         return p.getByteArray("",null);
+	     }
+	
+	 }
+
 }
