26cca634ff7e8f18f6b66fd9f340b3a159caa0bc
juanmelo25@gmail.com <juanmelo25@gmail.com@3a64d12f-38c2-6f80-d3d8-c9ae7da45978>
2010-10-04 00:02:01 +0000

    Generacion de reporte de ventas mensuales

--- aplicacion_web/src/ar/com/dailyMarket/services/MonthlySalesReportService.java
+++ aplicacion_web/src/ar/com/dailyMarket/services/MonthlySalesReportService.java
@@ -1,5 +1,122 @@
 package ar.com.dailyMarket.services;
 
-public class MonthlySalesReportService {
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
+
+import org.apache.commons.beanutils.DynaBean;
+
+import ar.com.dailyMarket.reports.ReportServiceImpl;
+
+public class MonthlySalesReportService extends ReportServiceImpl {
+	private static Map jaspers = new HashMap<String, JasperReport>();
+	private static JasperReport jasperReport = null;
+	
+	public int runReport(DynaBean reportData, OutputStream stream, String path, ServletContext ctx, Collection col, String report)throws Exception {
+        Map parameters = new HashMap();		
+        String imgs = "";
+        
+        Context initCtx;
+		try {			
+			initCtx = new InitialContext();
+			Context envCtx = (Context)initCtx.lookup("java:comp/env");
+	        imgs = (String) envCtx.lookup("reportsFolder");
+		} catch (NamingException e1) {
+			e1.printStackTrace();
+		}
+        //parameters.put("reportsFolder", imgs); 
+
+		try {
+			reportName="ventasMensuales";
+			if (jasperReport == null) {
+				jasperReport = JasperCompileManager.compileReport(this.getClass().getResourceAsStream(path + reportName + ".jrxml"));
+			}
+		
+			byte[] bytes = null;			
+			
+			bytes = JasperRunManager.runReportToPdf(jasperReport, parameters, getDataSource(col));			
+			
+			stream.write(bytes, 0, bytes.length);
+			stream.flush();
+			stream.close();
+			return bytes.length; //Para el setContentLength del response
+			
+			
+//			if (!(jaspers.containsKey(report))) {      	
+//        		//jaspers.put(report, JRLoader.loadObject(this.getClass().getResourceAsStream(path + report + ".jasper")));
+//        		report = "prueba";        		
+//        		jaspers.put(report, "C:\\workspace\\aplicacion_web\\config\\reports\\prueba.jrxml");
+////        		JasperCompileManager.compileReport("C:\\workspace\\aplicacion_web\\webapp\\WEB-INF\\classes\\reports\\ventasMensuales.jrxml");
+//        	}			
+//        	reportName="prueba";
+//        	jasperReport = JasperCompileManager.compileReport(this.getClass().getResourceAsStream(path + reportName + ".jrxml"));
+//        	byte[] bytes = JasperRunManager.runReportToPdf(jasperReport, parameters, getDataSource(col));						
+//			stream.write(bytes, 0, bytes.length);
+//			stream.flush();
+//			stream.close();
+//			return bytes.length; //Para el setContentLength del response
+        	
+        	//byte[]  bytes = JasperRunManager.runReportToPdf((String) jaspers.get(report), parameters, getDataSource(col));
+//			InputStream s = this.getClass().getClassLoader().getResourceAsStream("/reports/ventasMensuales.pdf");
+//			byte[] buffer = new byte[1000];
+//			int len;
+//			int tot = 0;
+//			
+//			while((len = s.read(buffer)) >= 0) {
+//				stream.write(buffer, 0, len);
+//				tot += len;
+//			}
+//				
+//			s.close();
+//			stream.flush();
+//			stream.close();
+			//return bytes; //Para el setContentLength del response
+			
+		} /*catch (JRException e) {
+			e.printStackTrace();
+			throw new Exception(e);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new Exception(e);
+		} */catch (Throwable e) {
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+	}
+    
+    private JRDataSource getDataSource(Collection results) {
+        return new CustomDS(results);
+    }
+    
+    protected class CustomDS implements JRDataSource  {		
+        protected Iterator<Object> iterator; 
+        protected Object currentValue; 
+        
+        public CustomDS(Collection c) {
+        	this.iterator = c.iterator();
+        }
+        
+        public Object getFieldValue(JRField field) {
+            return null;
+        }
+        
+        public boolean next() throws JRException { 
+            currentValue = iterator.hasNext() ? iterator.next() : null; 
+            return (currentValue != null);
+        }
+    }  
 }
--- aplicacion_web/webapp/pages/reportes/ventasMensualesFilter.jsp
+++ aplicacion_web/webapp/pages/reportes/ventasMensualesFilter.jsp
@@ -6,9 +6,9 @@
 <%@ taglib uri="/tags/displaytag" prefix="ds" %>
 
 
-<TABLE class="body"  border="0" cellpadding="0" cellspacing="0">	
+<TABLE class="form"  border="0" cellpadding="0" cellspacing="0">	
 	<TR> 
-		<TD> 
+		<TD colspan="7"> 
 			<ul class="errors" type="square">
 				<html:messages id="mensaje" message="true" >
 					<li><bean:write name="mensaje" /></li>
@@ -17,15 +17,79 @@
 		</TD>
 	</TR>
 	<tr>
-		<td>&nbsp;</td>
+		<td colspan="7">&nbsp;</td>
+	</tr>
+	<TR> 
+		<TD colspan="7">
+			<h1 class="formtitle"><bean:message key="reportes.ventasMensuales"/></h1>
+		</TD>		
+	</TR>	
+	<tr>
+		<td colspan="7">&nbsp;</td>
 	</tr>
 	<tr>
-		<td >
-			<table width="100%" align="left" cellpadding="0"  cellspacing="0" border="0">			
-				<tr align="center">
-					<td>FILTROS PARA REALIZAR UN REPORTE DE VENTAS MENSUALES</td>
-				</tr>
-			</table>
+		<td>&nbsp;</td>
+		<th><bean:message key="commons.yearDesde"/></th>
+		<td>&nbsp;</td>
+		<td>
+			<html:select property="yearFrom">
+				<html:option value="0">2009</html:option>
+			</html:select>
+		</td>
+		<th><bean:message key="commons.yearHasta"/></th>
+		<td>&nbsp;</td>
+		<td>
+			<html:select property="yearTo">
+				<html:option value="1">2010</html:option>
+			</html:select>
 		</td>
 	</tr>
+	<tr>
+		<td colspan="7">&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<th><bean:message key="commons.mesDesde"/></th>
+		<td>&nbsp;</td>
+		<td>
+			<html:select property="monthFrom">
+				<html:option value="0">Septiembre</html:option>
+			</html:select>
+		</td>
+		<th><bean:message key="commons.mesHasta"/></th>
+		<td>&nbsp;</td>
+		<td>
+			<html:select property="monthTo">
+				<html:option value="1">Febrero</html:option>
+			</html:select>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="7">&nbsp;</td>
+	</tr>
+	<TR>
+		<td>&nbsp;</td>
+		<TH><bean:message key="ProductForm.groupProduct"/></TH>
+		<td>&nbsp;</td>
+		<TD colspan="4">
+			<html:select property="groupProductId">						
+				<OPTION VALUE="">Seleccione</OPTION>
+				<html:options collection="groupsProduct" property="id" labelProperty="name" />				
+			</html:select>
+		</TD>
+	</TR>
+	<tr>
+		<td colspan="7">&nbsp;</td>
+	</tr>
+	<TR>
+		<td>&nbsp;</td>
+		<TH><bean:message key="reportes.ventasAnuales.producto"/></TH>
+		<td>&nbsp;</td>
+		<TD colspan="4">
+			<html:select property="productId">						
+				<OPTION VALUE="">Seleccione</OPTION>
+				<html:options collection="products" property="id" labelProperty="name" />				
+			</html:select>
+		</TD>
+	</TR>	
 </TABLE>
