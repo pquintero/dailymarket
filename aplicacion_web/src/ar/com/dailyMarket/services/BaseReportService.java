56cadbe724ee9c3ff950a05a400109118b50b09a
juanmelo25@gmail.com <juanmelo25@gmail.com@3a64d12f-38c2-6f80-d3d8-c9ae7da45978>
2010-10-11 21:03:33 +0000

    Generalizacion de los reportes de ventas mensuale y anuales en uno solo Generalizacin de los reportes de facturacion anual y mensual en uno solo Generacion de reportes de listados de codigos de barra (sin funcionar el codigo) y listado de precios

--- aplicacion_web/src/ar/com/dailyMarket/services/BaseReportService.java
+++ aplicacion_web/src/ar/com/dailyMarket/services/BaseReportService.java
@@ -1,9 +1,12 @@
 package ar.com.dailyMarket.services;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts.action.DynaActionForm;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
 import ar.com.dailyMarket.model.GroupProduct;
 import ar.com.dailyMarket.model.HourlyBand;
@@ -35,4 +38,34 @@ public class BaseReportService {
     	filters.put("hourlyBand", hourlyBand != null ? hourlyBand.getDetail() : "Todas");
     	return filters;
     }
+	
+	public Map<String,Object> getFiltersObject (DynaActionForm form) {
+    	ProductService productService = new ProductService();
+    	GroupProductService groupProductService = new GroupProductService();
+    	Product product = null;
+    	GroupProduct groupProduct = null;
+    	
+    	if (((Long)(form.get("productId"))).longValue() != new Long(-1).longValue()) {
+    		product = productService.getProductByPK((Long)form.get("productId"));
+    	}
+    	if (((Long)(form.get("groupProductId"))).longValue() != new Long(-1).longValue()) {
+    		groupProduct = groupProductService.getGroupProductByPK((Long)form.get("groupProductId"));
+    	}
+    	Map<String, Object> filters = new HashMap<String, Object>();
+    	filters.put("productFilter", product != null ? product : null);
+    	filters.put("groupProduct", groupProduct != null ? groupProduct : null);    	
+    	return filters;
+    }
+	
+	//PRE: los productos filtrados y/o grupo de producto estan activos en la base
+	@SuppressWarnings("unchecked")
+	public List<Product> getListProduct(Map<String,Object> filters) {		
+		Criteria c = HibernateHelper.currentSession().createCriteria(Product.class); 
+		if (filters.get("product") != null) {
+			c.add(Restrictions.eq("id", ((Product)filters.get("productFilter")).getId()));
+		} else if (filters.get("groupProduct") != null) {
+			c.add(Restrictions.eq("groupProduct.id", ((GroupProduct)filters.get("groupProduct")).getId()));
+		}
+		return c.list();
+	}
 }
