e590c8598ab1e50423c6c2ef3d4b130ac0beb627
juanmelo25@gmail.com <juanmelo25@gmail.com@3a64d12f-38c2-6f80-d3d8-c9ae7da45978>
2010-10-14 02:19:48 +0000

    Se modifico la pantalla del simulador de compras, y se hardcodearon los resultados a mostrar del simulador

--- trunk/aplicacion_web/src/ar/com/dailyMarket/ui/SimulatorAction.java
+++ trunk/aplicacion_web/src/ar/com/dailyMarket/ui/SimulatorAction.java
@@ -1,43 +1,44 @@
 package ar.com.dailyMarket.ui;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
 
+import ar.com.dailyMarket.model.Product;
 import ar.com.dailyMarket.services.GroupProductService;
+import ar.com.dailyMarket.services.ProductService;
+import ar.com.dailyMarket.services.SimulatorService;
 
 public class SimulatorAction extends BaseAction {
 	
 	public ActionForward initAction (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-		setGroupProductRequest(request);
+		setProductAndGroupProduct(request);
+		request.setAttribute("products", new ArrayList<Product>());
 		return mapping.findForward("showSimulator");
 	}
 	
 	public ActionForward executeSimulator (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//ejecutar simulador
-		//redireccionar a home con msj q el simulador ejecuto correctamente
-		return doHome(mapping, form, request, response);
-	}
-	
-	public ActionForward doHome (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		request.getSession().setAttribute("initMethod", true);
-		return mapping.findForward("doHome");
+		SimulatorService simulatorService = new SimulatorService();
+    	request.setAttribute("productsList", simulatorService.executeFilter((DynaActionForm)form));    	
+    	setProductAndGroupProduct(request);
+    	return mapping.findForward("showSimulator");
 	}
 	
 	public ActionForward stepBack (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {		
 		return mapping.findForward("redirectFilter");
     }
 	
-	public ActionForward saveAndBackToCheckPoint (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	//guardar el usuario
-    	return doHome(mapping, form, request, response);
-    }
-	
-	private void setGroupProductRequest (HttpServletRequest request) {
+	private void setProductAndGroupProduct (HttpServletRequest request) {
     	GroupProductService groupProductService = new GroupProductService();
+    	ProductService productService = new ProductService();
+    	
+    	request.setAttribute("products", productService.getAllProducts());
     	request.setAttribute("groupsProduct", groupProductService.getAllGroupProduct());
     }     
 }
