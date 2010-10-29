0edb4a45063e17a2070a0bca4f1ba4390b720d5a
epallich <epallich@3a64d12f-38c2-6f80-d3d8-c9ae7da45978>
2010-10-29 15:20:33 +0000

    simulador

--- aplicacion_web/src/ar/com/dailyMarket/ui/SimulatorAction.java
+++ aplicacion_web/src/ar/com/dailyMarket/ui/SimulatorAction.java
@@ -1,10 +1,12 @@
 package ar.com.dailyMarket.ui;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -18,6 +20,11 @@ import ar.com.dailyMarket.services.SimulatorService;
 public class SimulatorAction extends BaseAction {
 	
 	public ActionForward initAction (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		//TODO Inicializar todo el formulario
+		((DynaActionForm)form).set("margen","");
+		((DynaActionForm)form).set("yearFrom","2009");
+		((DynaActionForm)form).set("days",0);
+		((DynaActionForm)form).set("simuladorArray", new String[0]);
 		setProductAndGroupProduct(request);
 		request.setAttribute("productsList", new ArrayList<Product>());
 		return mapping.findForward("showSimulator");
@@ -25,16 +32,38 @@ public class SimulatorAction extends BaseAction {
 	
     public ActionForward executeFilter (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
     	SimulatorService simulatorService = new SimulatorService();
-    	request.setAttribute("productsList", simulatorService.executeFilter((DynaActionForm)form));    	
+    	List<Product> productList = simulatorService.executeFilter((DynaActionForm)form);
+    	request.setAttribute("productsList", productList);
+    	
+    	String [] array = new String[productList.size()];
+    	for (int i = 0; i < array.length; i++) {
+			array[i]="";
+		}
+    	
+    	((DynaActionForm)form).set("simulatedSizeOfPurchaseArray", array);
+    	((DynaActionForm)form).set("simulatedRepositionStockArray", array);
+    	
     	setProductAndGroupProduct(request);
     	return mapping.findForward("showSimulator");
     }
 	
 	public ActionForward executeSimulator (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (!StringUtils.isNotEmpty((String) ((DynaActionForm)form).get("margen"))) {
+			return executeFilter(mapping, form, request, response);
+		}
 		SimulatorService simulatorService = new SimulatorService();
-    	request.setAttribute("productsList", simulatorService.executeFilter((DynaActionForm)form));    	
+		request.setAttribute("productsList", simulatorService.executeFilter((DynaActionForm)form));
+		
+		simulatorService.executeSimulation((DynaActionForm)form);
+		
     	setProductAndGroupProduct(request);
     	return mapping.findForward("showSimulator");
+	}
+	
+	public ActionForward aplicarCambios (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		SimulatorService simulatorService = new SimulatorService();
+		simulatorService.aplicarCambios(form);
+		return initAction(mapping, form, request, response);
 	}
 	
 	public ActionForward stepBack (ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {		
